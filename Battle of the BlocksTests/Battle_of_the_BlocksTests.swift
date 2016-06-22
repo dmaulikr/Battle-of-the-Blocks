221d15c777ac36f598583180ccde8c4e61dde7c4
Accelerator Beast <AcceleratorBeast@lives-imac-2.local>
2016-06-22 09:24:36 -0700

    Initial Commit

---
+++ Battle of the BlocksTests/Battle_of_the_BlocksTests.swift
@@ -0,0 +1,36 @@
+//
+//  Battle_of_the_BlocksTests.swift
+//  Battle of the BlocksTests
+//
+//  Created by Accelerator Beast on 6/22/16.
+//  Copyright © 2016 Olatayo S. All rights reserved.
+//
+
+import XCTest
+@testable import Battle_of_the_Blocks
+
+class Battle_of_the_BlocksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
